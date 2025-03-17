d1940e7289b7d2cdb92d3a4074d3b244f95f171c
Filipkekw <fkowalczyk780@gmail.com>
2025-03-17 12:58:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import tkinter as tk
 from tkinter import messagebox
 
+# Funkcja dodająca zadanie do listy
+def add_to_list():
+    text = task_entry.get()  # Pobiera tekst z pola tekstowego
+    if text != "":  # Jeżeli tekst nie jest pusty
+        task_listbox.insert(tk.END, text)  # Dodaje tekst do listy
+        task_entry.delete(0, tk.END)  # Czyści pole tekstowe
+    else:
+        messagebox.showwarning("Uwaga!", "Nie można dodać pustego zadania!")
+
 # Tworzenie głównego okna
 root = tk.Tk()
 root.title("To-Do List")
@@ -10,9 +19,13 @@ root.geometry("600x400")
 task_entry = tk.Entry(root, width=50)
 task_entry.pack(pady=10)
 
-# Przycisk do dodawania zadań
-add_button = tk.Button(root, text="+", width=10)
+# Przycisk do dodawania i usuwania zadań (tylko grafika)
+add_button = tk.Button(root, text="Dodaj zadanie", width=12, command=add_to_list)
 add_button.pack(pady=5)
+
+# Lista zadań
+task_listbox = tk.Listbox(root, width=50, height=15, selectmode=tk.SINGLE, selectbackground="#a6a6a6")
+task_listbox.pack(pady=10)
 
 # Uruchamianie aplikacji
 root.mainloop()
